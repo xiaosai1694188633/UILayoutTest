c22d66c72ecfbf12cdee90e03a95cb0c151ea380
han <1694188633@qq.com>
2016-03-01 17:01:21 +0800

    四种布局方式测试项目 git commit -m 四种布局方式测试项目

---
+++ app/src/main/java/com/xiaosai/uilayouttest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xiaosai.uilayouttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/3/1 0001.
+ */
+public class MainActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
